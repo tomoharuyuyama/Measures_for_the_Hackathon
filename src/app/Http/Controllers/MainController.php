9f308ba51965618ae8f2c97da9385710982d7c8a
tomoharuyuyama <tomoharuyuyama@gmail.com>
2022-02-12 19:06:52 +0900

    リレーション確認

--- src/app/Http/Controllers/MainController.php
+++ src/app/Http/Controllers/MainController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Capa;
+use App\Models\Task;
 
 class MainController extends Controller
 {
   public function shop()
   {
-    return view('shop');
+    $capas = Capa::all();
+    $tasks = Task::all();
+    dd($tasks);
+    return view('shop', compact('tasks', 'capas'));
   }
   
   public function mypage()
   {
-    return view('mypage');
+    $capas = Capa::with('user')->first();
+    $tasks = Task::all();
+    dd($capas->user->name);
+    return view('mypage', compact('tasks', 'capas'));
   }
 }
