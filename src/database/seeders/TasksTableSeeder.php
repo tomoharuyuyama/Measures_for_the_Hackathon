f1acf8789b6b130baf7169e372c34860a1a8365f
ryudai <bigdragon.06102001@gmail.com>
2022-02-13 01:03:40 +0900

    tasksテーブルにstatusカラムを追加

--- src/database/seeders/TasksTableSeeder.php
+++ src/database/seeders/TasksTableSeeder.php
@@ -18,24 +18,28 @@ class TasksTableSeeder extends Seeder
             'dead_line' => '20801122',
             'cost_capa' => 20,
             'user_id' => 0,
+            'status' => 0,
         ]);
         DB::table('tasks')->insert([
             'title' => '釣り',
             'dead_line' => '20021122',
             'cost_capa' => 40,
             'user_id' => 0,
+            'status' => 0,
         ]);
         DB::table('tasks')->insert([
             'title' => '大根を買う',
             'dead_line' => '20101122',
             'cost_capa' => 10,
             'user_id' => 0,
+            'status' => 0,
         ]);
         DB::table('tasks')->insert([
             'title' => '私のみ表示される',
             'dead_line' => '20101122',
             'cost_capa' => 10,
             'user_id' => 0,
+            'status' => 0,
         ]);
     }
 }
